9bdc03a9edb4471803ac269717b1e45079eb052a
Michal Smaluch <smaluchmichal@gmail.com>
2023-01-16 11:59:09 +0100

    x

---
+++ .cache/wal/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#c9bcb7", "#160814" },
+	[SchemeSel] = { "#c9bcb7", "#5F4968" },
+	[SchemeOut] = { "#c9bcb7", "#A25EC4" },
+};
