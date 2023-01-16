bf739ffa4d836e801d10823b48ce796da5a81eac
Michal Smaluch <smaluchmichal@gmail.com>
2023-01-16 12:00:25 +0100

    x

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c9bcb7", "#160814" },
-	[SchemeSel] = { "#c9bcb7", "#5F4968" },
-	[SchemeOut] = { "#c9bcb7", "#A25EC4" },
-};
--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#160814";
-static const char* selfgcolor   = "#c9bcb7";
-static const char* normbgcolor  = "#9F2651";
-static const char* normfgcolor  = "#c9bcb7";
-static const char* urgbgcolor   = "#5F4968";
-static const char* urgfgcolor   = "#c9bcb7";
